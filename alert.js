219c239351ee67876f9e24116b24fe0e2a6d4fae
Txen23 <33638854+Txen23@users.noreply.github.com>
2017-12-04 19:30:02 +0300

    Add a button 'Square a number'

--- alert.js
+++ alert.js
@@ -7,3 +7,7 @@ function alertButton(){
 function alertCat(){
     alert ('Okay, stop it')
 }
+//document.getElementById('inpSquare').value
+function squareNumber(){
+    alert (document.getElementById('inpSquare').value*document.getElementById('inpSquare').value)
+}
